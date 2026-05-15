cfc4de008f3607048eb98f6bf79847e27a106231
Lucia Lemes <lemeslucya@gmail.com>
2026-05-15 14:23:24 -0300

    feat: agregar módulo app.js, actualizar server.js y agregar .env.example

---
+++ app.js
@@ -0,0 +1,27 @@
+// src/app.js
+const express = require('express');
+const authorsRouter = require('./routes/authors');
+const postsRouter = require('./routes/posts');
+const errorHandler = require('./middlewares/errorHandler');
+
+const app = express();
+
+app.use(express.json());
+
+app.use('/posts', postsRouter);
+app.use('/authors', authorsRouter);
+
+app.get('/', (req, res) => {
+    res.json({
+        message: 'Blog API',
+        endpoints: {
+            authors: '/authors',
+            posts: '/posts',
+            postsByAuthor: '/posts/author/:authorId'
+        }
+    });
+});
+
+app.use(errorHandler);
+
+module.exports = app;
--- server.js
+++ server.js
@@ -1,37 +1,9 @@
-const express = require ('express');
-const authorsRouter = require('./routes/authors.js');
-const postsRouter = require('./routes/posts.js');
-
+// src/server.js
 require('dotenv').config();
+const app = require('./app'); // importamos la app configurada
 
-const app = express();
 const PORT = process.env.PORT || 3000;
-
-app.use(express.json());
-app.use('/posts', postsRouter);
-app.use('/authors', authorsRouter);
-
-app.get('/', (req, res) => {
-    res.json ({
-        message: 'Blog API',
-        endpoints: {
-            authors: '/api/authors',
-            posts: '/api/posts'
-        }
-    })
-})
-
-app.get('/', (req, res) => {
-    res.json({
-        message: 'Blog API',
-        endpoints: {
-            authors: '/authors',
-            posts: '/posts',
-            postsByAuthor: '/posts/author/:authorId'
-        }
-    })
-});
 
 app.listen(PORT, () => {
     console.log(`Blog API escuchando en http://localhost:${PORT}`);
-})
+});
